d74ce6f4736430b24449a8eb879b10d5aac7ae0c
Steven Toth <stoth@kernellabs.com>
2017-03-09 15:16:11 -0500

    Added a startup console line to show the X264 build# and 8/10bit build configuration

--- obe/obecli.c
+++ obe/obecli.c
@@ -1574,6 +1574,7 @@ static void _usage(const char *prog, int exitcode)
     printf("\nOpen Broadcast Encoder command line interface.\n");
     printf("Including Kernel Labs enhancements.\n");
     printf("Version 1.5 (" GIT_VERSION ")\n");
+    printf("x264 build#%d (%dbit support)\n", X264_BUILD, X264_BIT_DEPTH);
     printf("\n");
 
     if (exitcode) {
